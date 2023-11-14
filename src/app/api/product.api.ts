2b5f3ee3ace3b472af68ef0583377916249727b5
abbasmashaddy72 <abbasmashaddy72@gmail.com>
2023-11-14 11:55:28 +0530

    Added Edit Modal of Product with get API

--- src/app/api/product.api.ts
+++ src/app/api/product.api.ts
@@ -21,4 +21,18 @@ export class ProductService {
       { headers: header }
     )
   }
+
+  getProductById (cls: object) {
+    const header = new HttpHeaders()
+      .set('content-type', 'application/json')
+      .set('Access-Control-Allow-Origin', '*')
+
+    return this.httpClient.post(
+      Constants.SAYDAK_API_ENDPOINT +
+        Constants.API_VERSION +
+        '/Admin/_getProductById',
+      cls,
+      { headers: header }
+    )
+  }
 }
--- src/app/edit/modal-product/modal-product.component.ts
+++ src/app/edit/modal-product/modal-product.component.ts
@@ -1,17 +1,50 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+import { ProductService } from 'src/app/api/product.api'
 import { ModalService } from 'src/app/modal-service.service'
+import { AuthenticationService } from 'src/app/services/authentication.service'
 
 @Component({
   selector: 'app-modal-product',
   templateUrl: './modal-product.component.html',
   styleUrls: ['./modal-product.component.scss']
 })
-export class ModalProductComponent {
-  constructor (public modalService: ModalService) {}
+export class ModalProductComponent implements OnInit {
   @Input() editedProduct: any
+  productData: any
+
+  constructor (
+    private authenticationService: AuthenticationService,
+    private productService: ProductService,
+    public modalService: ModalService
+  ) {}
+
+  ngOnInit (): void {
+    this.productById()
+  }
+
+  productById () {
+    var request = {
+      seqno: this.editedProduct,
+      UserId: this.authenticationService.getToken().usersUserid + 1,
+      ClientId: this.authenticationService.getToken().usersClientId
+    }
+    this.productService.getProductById(request).subscribe(
+      (response: any) => {
+        if (response['status'] == '200') {
+          this.productData = response['objresult']
+        } else {
+          alert('failure')
+          console.log('Error Fetching the API Data')
+        }
+      },
+      error => {
+        console.error('API Error:', error)
+      }
+    )
+  }
 
   saveProduct () {
-    console.log('Saved')
-    return true
+    alert('Saved')
+    this.modalService.closeModal()
   }
 }
